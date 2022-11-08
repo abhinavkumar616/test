d832a9b38ad90e6aee6b228e1c7b167025567881
Abhinav <abhinavkumar616@gmail.com>
2022-11-08 19:39:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http=require("http")
+// const app=express()
+
+var server=http.createServer(function (req,res){
+    // const get("/",(req,res)=>{
+
+    // })
+})
+
+server.listen(8000,()=>console.log("server is running on port 8000"));
